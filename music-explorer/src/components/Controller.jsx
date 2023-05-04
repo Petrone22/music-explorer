7b3bab5c2c50e9d7c8faaf90800cf792f61ccf60
Petrone22 <the1rashid@gmai.com>
2023-05-04 19:08:53 +0300

    updated song selection functionality

--- music-explorer/src/components/Controller.jsx
+++ music-explorer/src/components/Controller.jsx
@@ -1,6 +1,6 @@
 // big bg, small bg, description, play song, page title, page subtitle
 
-import React, { useEffect } from "react";
+import React, { useRef, useCallback } from "react";
 import play from "../assets/playMain.svg";
 import pause from "../assets/pause.svg";
 import backbtn from "../assets/back.svg";
@@ -8,19 +8,25 @@ import frontbtn from "../assets/front.svg";
 import volume from "../assets/volume.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMediaIsPlaying } from "../state/isPlayingSlice";
-const Controller = () => {
-  useEffect(() => {
-    const audioPlayer = new Audio();
-  });
 
+const Controller = ({ audioRef, currentTime }) => {
   const title = useSelector((state) => state.title.value);
   const subTitle = useSelector((state) => state.subTitle.value);
   const mediaIsPlaying = useSelector((state) => state.mediaIsPlaying.value);
   const smallBG = useSelector((state) => state.smallBG.value);
   const runtime = useSelector((state) => state.runtime.value);
   const dispatch = useDispatch();
+  const playAnimationRef = useRef();
+  const progressBarRef = useRef();
+  console.log(currentTime);
   return (
-    <div className="rounded-md flex items-center justify- mb-4 lg:justify-center gap-6">
+    <div
+      className="rounded-md flex items-center justify- mb-4 lg:justify-center gap-6"
+      style={{
+        pointerEvents: title === "Music Explorer" ? "none" : "auto",
+        opacity: title === "Music Explorer" ? "0.4" : "1",
+      }}
+    >
       <div className=" rounded-md p-2 top-4 lg:flex gap-2 items-center ">
         <img src={smallBG} className="w-14 rounded-md " alt="background" />
         <div className="flex flex-col justify-end">
@@ -33,18 +39,40 @@ const Controller = () => {
       <div className="w-full flex flex-col ">
         <div className="flex gap-6 p-2 items-center justify-center">
           <img src={backbtn} alt="back" className="active:scale-95" />
+
           <img
             src={mediaIsPlaying ? pause : play}
             alt="play pause"
             className="active:scale-95 m-2"
             onClick={() => {
-              dispatch(toggleMediaIsPlaying());
+              {
+                mediaIsPlaying
+                  ? audioRef.current.pause()
+                  : audioRef.current.play();
+              }
+              mediaIsPlaying
+                ? dispatch(toggleMediaIsPlaying(false))
+                : dispatch(toggleMediaIsPlaying(true));
             }}
           />
-          <img src={frontbtn} alt="front" className="active:scale-95" />
+          <img
+            src={frontbtn}
+            alt="front"
+            className="active:scale-95"
+            onClick={() => {}}
+          />
         </div>
         <div className="flex gap-6  items-center justify-center relative">
-          <input type="range" className="w-10/12" />
+          <input
+            type="range"
+            className="w-10/12"
+            max={audioRef.current ? audioRef.current.duration : "0"}
+            step={1}
+            value={audioRef.current ? audioRef.current.currentTime : "0"}
+            onChange={(e) => {
+              audioRef.current.currentTime = e.target.value;
+            }}
+          />
           <span className="absolute font-thin text-md -right-3 lg:font-normal lg:text-base lg:right-8 ">
             {runtime}
           </span>
@@ -63,7 +91,7 @@ const Controller = () => {
             max={1}
             step={0.1}
             onChange={(e) => {
-              console.log(e.target.value);
+              audioRef.current.volume = e.target.value;
             }}
           />
         </div>
